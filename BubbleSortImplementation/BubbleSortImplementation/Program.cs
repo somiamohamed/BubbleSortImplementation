8764ffd8113a3b4c228e30a3a0a71a42b84613d2
Somaya Mohamed <118680203+somiamohamed@users.noreply.github.com>
2024-04-21 11:51:16 +0200

    Add files via upload

---
+++ BubbleSortImplementation/BubbleSortImplementation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 5, 3, 8, 4, 2 };
+        BubbleSort(arr);
+
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    public static void BubbleSort<T>(T[] arr, IComparer<T> comparer = null) where T : IComparable
+    {
+        if (comparer == null)
+        {
+            comparer = Comparer<T>.Default;
+        }
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = 0; j < arr.Length - 1 - i; j++)
+            {
+                if (comparer.Compare(arr[j], arr[j + 1]) > 0)
+                {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
